697b8032c295e1b723a8ee879b9212dfa32014f4
Chris Murphy <61854922+cmannnn@users.noreply.github.com>
2021-12-24 08:28:18 -0500

    Create 4.47_car_value_2.java

---
+++ week_4/4.47_car_value_2.java
@@ -0,0 +1,38 @@
+public class Car {
+   private int modelYear; 
+   private int purchasePrice;
+   private int currentValue;
+   
+   public void setModelYear(int userYear){
+      modelYear = userYear;
+   }
+   
+   public int getModelYear() {
+      return modelYear;
+   }
+   
+   public void setPurchasePrice(int userPrice) {
+      purchasePrice = userPrice;   
+   }
+   
+   public int getPurchasePrice() {
+      return purchasePrice;   
+   }
+   
+   public void calcCurrentValue(int currentYear) {
+      double depreciationRate = 0.15;
+      int carAge = currentYear - modelYear;
+      
+      // Car depreciation formula
+      currentValue = (int) 
+         Math.round(purchasePrice * Math.pow((1 - depreciationRate), carAge));
+   }
+   
+   // TODO: Define printInfo() method to output modelYear, purchasePrice, and currentValue
+   public void printInfo() {
+        System.out.println("Car's information:\n" +
+                            "   Model year: " + modelYear + "\n" +
+                            "   Purchase price: " + purchasePrice + "\n" + 
+                            "   Current value: " + currentValue);
+   }
+}
