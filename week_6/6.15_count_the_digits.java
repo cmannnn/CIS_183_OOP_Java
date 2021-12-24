d9316565e6f3675b5b48f66fec3aa265e8dd372c
Chris Murphy <61854922+cmannnn@users.noreply.github.com>
2021-12-24 08:38:53 -0500

    Create 6.15_count_the_digits.java

---
+++ week_6/6.15_count_the_digits.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class LabProgram {
+   
+   /* TODO: Write recursive digitCount() method here. */
+   public static int digitCount(int num) {
+      if (num < 10) {
+         return 1;
+      }
+      else {
+         return 1 + (digitCount(num / 10));   
+      }
+   
+   }
+   
+   
+   
+   
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in);
+      int num, digits;
+      
+      num = scnr.nextInt();
+      digits = digitCount(num);
+      System.out.println(digits);
+   }
+}
