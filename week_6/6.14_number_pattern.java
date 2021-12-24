2b4ef0e4bdb2e632508ad54528e7c4fa8c593032
Chris Murphy <61854922+cmannnn@users.noreply.github.com>
2021-12-24 08:37:56 -0500

    Create 6.14_number_pattern.java

---
+++ week_6/6.14_number_pattern.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class NumberPattern {
+   // TODO: Write recursive printNumPattern() method
+   public static void printNumPattern(int num1, int num2) {
+      System.out.print(num1 + " ");
+      if (num1 <= 0) {
+         return;
+      }
+      else {
+         printNumPattern(num1 - num2, num2);
+      }
+      
+      System.out.print(num1 + " ");
+  }
+  
+   public static void main(String[] args) { 
+      Scanner scnr = new Scanner(System.in);
+      int num1;
+      int num2;
+      
+      num1 = scnr.nextInt();
+      num2 = scnr.nextInt();
+      printNumPattern(num1, num2); 
+   } 
+} 
