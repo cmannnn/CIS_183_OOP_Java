620caee4b49e3af5458e2b321b94d4fc52145591
Chris Murphy <61854922+cmannnn@users.noreply.github.com>
2021-12-26 07:57:26 -0500

    Create 22.4_student_class_1.java

---
+++ week_8/22.4_student_class_1.java
@@ -0,0 +1,10 @@
+public class LabProgram {
+   public static void main(String[] args) {
+      Student student = new Student();
+      System.out.println(student.getName() + "/" + student.getGPA());
+      
+      student.setName("Felix");
+      student.setGPA(3.7);
+      System.out.println(student.getName() + "/" + student.getGPA());
+   }
+}
